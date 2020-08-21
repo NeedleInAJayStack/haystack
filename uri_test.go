e65558ba2860be2ff8e91aafaf911a416e2d153f
Jay Herron <jherron@buildingfit.com>
2020-08-20 22:44:33 -0600

    feature: Forgot to save a file

--- uri_test.go
+++ uri_test.go
@@ -3,9 +3,9 @@ package haystack
 import "testing"
 
 func TestUri_toZinc(t *testing.T) {
-	uri := Uri{val: "http://www.projecthaystack.org"}
+	uri := Uri{val: "http://www.project-haystack.org"}
 	got := uri.toZinc()
-	if got != "`http://www.projecthaystack.org`" {
+	if got != "`http://www.project-haystack.org`" {
 		t.Error(got)
 	}
 }

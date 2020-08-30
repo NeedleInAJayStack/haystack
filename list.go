b03e8c5696975aa57f79e739ec4a38e20cea08cd
Jay Herron <jherron@buildingfit.com>
2020-08-30 16:45:22 -0600

    feature: Adds list type

---
+++ list.go
@@ -0,0 +1,19 @@
+package haystack
+
+type List struct {
+	items []Val
+}
+
+// Convert object to zinc.
+func (list *List) toZinc() string {
+	result := "["
+	for i := 0; i < len(list.items); i++ {
+		if i > 0 {
+			result = result + ","
+		}
+		item := list.items[i]
+		result = result + item.toZinc()
+	}
+	result = result + "]"
+	return result
+}
---
+++ list_test.go
@@ -0,0 +1,17 @@
+package haystack
+
+import "testing"
+
+func TestList_toZinc(t *testing.T) {
+	list := List{
+		items: []Val{
+			&Number{val: 5.5},
+			&Time{hour: 23, min: 7, sec: 10},
+			&Ref{val: "null"},
+		},
+	}
+	listZinc := list.toZinc()
+	if listZinc != "[5.5,23:07:10,@null]" {
+		t.Error(listZinc)
+	}
+}

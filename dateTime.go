bdbf2829c2f48e709a4366ebefecc21c11db07eb
Jay Herron <jherron@buildingfit.com>
2020-09-06 18:31:48 -0600

    feature: dateTime now has timezone support

--- dateTime.go
+++ dateTime.go
@@ -1,21 +1,81 @@
 package haystack
 
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
 type DateTime struct {
-	date Date
-	time Time
-	tz   string
+	date     Date
+	time     Time
+	tz       string // IANA database city name
+	tzOffset int    // offset in seconds from UTC
+}
+
+func dateTimeFromGo(goTime time.Time) DateTime {
+	hDate := Date{
+		year:  goTime.Year(),
+		month: int(goTime.Month()),
+		day:   goTime.Day(),
+	}
+	hTime := Time{
+		hour: goTime.Hour(),
+		min:  goTime.Minute(),
+		sec:  goTime.Second(),
+		ms:   goTime.Nanosecond() / 1000,
+	}
+	location := goTime.Location()
+	hTz := "UTC"
+	if location != time.UTC {
+		tzName := goTime.Location().String()
+		hTz = strings.Split(tzName, "/")[1] // Don't include the region, only the city.
+	}
+	_, hTzOffset := goTime.Zone()
+
+	return DateTime{
+		date:     hDate,
+		time:     hTime,
+		tz:       hTz,
+		tzOffset: hTzOffset,
+	}
 }
 
 // Format is YYYY-MM-DD'T'hh:mm:ss.FFFz zzzz
 func (dateTime *DateTime) toZinc() string {
-	return dateTime.encode()
+	buf := strings.Builder{}
+	dateTime.encodeTo(&buf)
+	return buf.String()
 }
 
-func (dateTime *DateTime) encode() string {
-	result := ""
-	result = result + dateTime.date.encode()
-	result = result + "T"
-	result = result + dateTime.time.encode()
+func (dateTime *DateTime) encodeTo(buf *strings.Builder) {
+	buf.WriteString(dateTime.date.encode())
+	buf.WriteRune('T')
+	buf.WriteString(dateTime.time.encode())
+	if dateTime.tzOffset == 0 {
+		buf.WriteRune('Z')
+	} else {
+		offset := dateTime.tzOffset
+		if offset < 0 {
+			buf.WriteRune('-')
+			offset = offset * -1
+		} else {
+			buf.WriteRune('+')
+		}
+		hr := offset / 3600
+		min := (offset % 3600) / 60
 
-	return result
+		if hr < 10 {
+			buf.WriteRune('0')
+		}
+		buf.WriteString(fmt.Sprintf("%d", hr))
+		buf.WriteRune(':')
+
+		if min < 10 {
+			buf.WriteRune('0')
+		}
+		buf.WriteString(fmt.Sprintf("%d", min))
+	}
+	buf.WriteRune(' ')
+	buf.WriteString(dateTime.tz)
 }
--- dateTime_test.go
+++ dateTime_test.go
@@ -1,13 +1,28 @@
 package haystack
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestDateTime_toZinc(t *testing.T) {
-	date := Date{year: 2020, month: 8, day: 17}
-	time := Time{hour: 23, min: 7, sec: 10}
-	dateTime := DateTime{date: date, time: time}
-	dateTimeZinc := dateTime.toZinc()
-	if dateTimeZinc != "2020-08-17T23:07:10" {
-		t.Error(dateTimeZinc)
+	utc := dateTimeFromGo(time.Date(2020, time.August, 17, 23, 7, 10, 0, time.UTC))
+	utcZinc := utc.toZinc()
+	if utcZinc != "2020-08-17T23:07:10Z UTC" {
+		t.Error(utcZinc)
+	}
+
+	losAngelesLoc, _ := time.LoadLocation("America/Los_Angeles")
+	la := dateTimeFromGo(time.Date(2020, time.August, 17, 23, 7, 10, 0, losAngelesLoc))
+	laZinc := la.toZinc()
+	if laZinc != "2020-08-17T23:07:10-07:00 Los_Angeles" {
+		t.Error(laZinc)
+	}
+
+	taipeiLoc, _ := time.LoadLocation("Asia/Taipei")
+	taipei := dateTimeFromGo(time.Date(2020, time.August, 17, 23, 7, 10, 0, taipeiLoc))
+	taipeiZinc := taipei.toZinc()
+	if taipeiZinc != "2020-08-17T23:07:10+08:00 Taipei" {
+		t.Error(taipeiZinc)
 	}
 }

420f0123e57c046ebda5dae610f3399657fb5283
Jay Herron <NeedleInAJayStack@protonmail.com>
2023-03-26 23:51:24 -0600

    fix: Makes Error type properties public

--- client/Client.go
+++ client/Client.go
@@ -540,31 +540,31 @@ func (clientHTTP *clientHTTPImpl) prepare(req *http.Request, auth string) {
 
 // AuthError represents an error in the authorization
 type AuthError struct {
-	message string
+	Message string
 }
 
 // NewAuthError creates a new AuthError object.
 func NewAuthError(message string) AuthError {
-	return AuthError{message: message}
+	return AuthError{Message: message}
 }
 
 func (err AuthError) Error() string {
-	return "Auth error: " + err.message
+	return "Auth error: " + err.Message
 }
 
 // CallError occurs when communication is successful, and a Grid is returned, but the grid has an err
 // tag indicating a server side error.
 type CallError struct {
-	grid haystack.Grid
+	Grid haystack.Grid
 }
 
 // NewCallError creates a new CallError object.
 func NewCallError(grid haystack.Grid) CallError {
-	return CallError{grid: grid}
+	return CallError{Grid: grid}
 }
 
 func (err CallError) Error() string {
-	dis := err.grid.Meta().Get("dis")
+	dis := err.Grid.Meta().Get("dis")
 	switch val := dis.(type) {
 	case haystack.Str:
 		return "Call error: " + val.String()
@@ -575,29 +575,29 @@ func (err CallError) Error() string {
 
 // HTTPError occurs when communication is successful with a server, but we receive an HTTP error response.
 type HTTPError struct {
-	code    int
-	message string
+	Code    int
+	Message string
 }
 
 // NewHTTPError creates a new HTTPError object.
 func NewHTTPError(code int, message string) HTTPError {
-	return HTTPError{code: code, message: message}
+	return HTTPError{Code: code, Message: message}
 }
 
 func (err HTTPError) Error() string {
-	return "HTTP error: " + err.message
+	return "HTTP error: " + err.Message
 }
 
 // NetworkError occurs when there is a network I/O or connection problem with communication to the server.
 type NetworkError struct {
-	message string
+	Message string
 }
 
 // NewNetworkError creates a new NetworkError object.
 func NewNetworkError(message string) NetworkError {
-	return NetworkError{message: message}
+	return NetworkError{Message: message}
 }
 
 func (err NetworkError) Error() string {
-	return "Network error: " + err.message
+	return "Network error: " + err.Message
 }
